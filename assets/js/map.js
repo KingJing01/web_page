7da1b3705e287d51aec2930028991cd3d5f1002c
KingJing01 <18049808631@163.com>
2019-04-19 10:21:39 +0800

    企业介绍地图信息修改

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,27 +1,12 @@
-// 百度地图API功能
-// var map = new BMap.Map("allmap");
-// var point = new BMap.Point(116.404, 39.915);
-// map.centerAndZoom(point, 15);
-
-// //创建小狐狸
-// var pt = new BMap.Point(121.569248,31.212491);
-// var myIcon = new BMap.Icon(".", new BMap.Size(300,157));
-// var marker2 = new BMap.Marker(pt,{icon:myIcon});  // 创建标注
-// map.addOverlay(marker2);
-
-// 百度地图API功能
-var map = new BMap.Map("allmap");
-var point = new BMap.Point(121.569248,31.212491);
-map.centerAndZoom(point, 15);
-
-//创建小狐狸
-var pt = new BMap.Point(121.569248,31.212491);
-var myIcon = new BMap.Icon("../assets/img/map111.gif", new BMap.Size(300,157));
-var marker2 = new BMap.Marker(pt,{icon:myIcon});  // 创建标注
-map.addOverlay(marker2); 
-
-// 创建信息
-var pt = new BMap.Point(121.569248,31.212491);
-var myIconCn = new BMap.Icon("../assets/img/ic_gs.svg", new BMap.Size(465, 200));
-var marker3 = new BMap.Marker(pt,{icon:myIconCn});  // 创建标注
-map.addOverlay(marker3); 
+!function(){
+    var map = new BMap.Map("allmap");    
+    var point = new BMap.Point(121.569248,31.212491);
+    map.centerAndZoom(point, 15); 
+    var myIcon = new BMap.Icon("../assets/img/map111.gif",new BMap.Size(50,50));
+    var marker = new BMap.Marker(point,{icon:myIcon});  
+    map.addOverlay(marker);
+    var myIconCn = new BMap.Icon("../assets/img/ic_gs.svg", new BMap.Size(200, 120));
+    var marker3 = new BMap.Marker(point,{icon:myIconCn});  // 创建标注
+    map.addOverlay(marker3); 
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+}();
